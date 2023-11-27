eeb1402a48135f191293af3a16a769b691c0c067
Pape Maguette Ndiaye <papecheikh1102@gmail.com>
2023-11-27 23:26:06 +0000

    mis en page de ma todolist

--- TodoList/app.js
+++ TodoList/app.js
@@ -1,6 +1,84 @@
-function Welcome(props){
-    const valeur = 2
-    return  <h1>Bonjour {props.name} {valeur}</h1>
+class Todo extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            userInput: '',
+            tache: []
+        }
+        this.onChange = this.onChange.bind(this)
+        this.addTodo = this.addTodo.bind(this)
+        this.deleteTodo = this.deleteTodo.bind(this)
+        this.editTodo = this.editTodo.bind(this)
+    }
+
+    onChange(e) {  
+        this.setState({
+            userInput: e.target.value
+        })
+    }
+
+    addTodo(e) {
+        e.preventDefault()
+        this.setState({
+            userInput:'',
+            tache: [...this.state.tache, this.state.userInput]
+        })
+    }
+
+    deleteTodo(e) {
+        e.preventDefault()
+        const tableau = this.state.tache
+        const index = tableau.indexOf(e.target.value)
+        tableau.splice(index, 1)
+        this.setState({
+            tache:tableau
+        })
+    }
+
+    editTodo(e) {
+        e.preventDefault()
+
+    }
+
+    renderTodo() {
+        return this.state.tache.map((maTache) => {
+            return(
+                <div 
+                key={maTache} 
+                className="container list-group-item ">
+                    <div className="d-flex">
+                        <div className='col-7'>
+                            {maTache} 
+                        </div>
+                     <button onClick ={this.deleteTodo} className='btn btn-danger mx-1'>Supprimer</button>
+                     <button onClick ={this.editTodo} className='btn btn-warning mx-1'>Modifier</button>
+                    </div>
+                </div>
+            )
+        })
+    }
+   
+
+    render() {
+        return <div>
+            <h1 className="text-center my-5">Ma TodoList</h1>
+                <form className="container w-50">
+                <input type='text' value ={this.state.userInput} onChange={this.onChange} className= 'form-control' placeholder="Ajouter une tache"/>
+                <div className= 'd-flex justify-content-center'>
+                    <button onClick ={this.addTodo} className="btn btn-primary  my-2 ">Ajouter</button>
+                </div>
+                {/* {JSON.stringify(this.state)} */}
+            </form>
+            <div>
+                {this.renderTodo()}
+            </div>
+        </div> 
+     
+    }
 }
 
-ReactDOM.render(<Welcome name ="Pape Cheikh" />, document.getElementById('root'))
+
+    ReactDOM.render(<Todo />, document.getElementById('root'))
+
+
+
