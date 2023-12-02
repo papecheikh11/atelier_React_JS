a1591779313452ba1b1893d63d5f6f45e718021a
Pape Maguette Ndiaye <papecheikh1102@gmail.com>
2023-12-02 01:30:28 +0000

    differents composants

--- TodoList/app.js
+++ TodoList/app.js
@@ -1,102 +1,125 @@
-class Todo extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            userInput: '',
-            tache: [],
-        }
-        this.addTodo = this.addTodo.bind(this)
-        this.deleteTodo = this.deleteTodo.bind(this)
-        this.editTodo = this.editTodo.bind(this)
-    }
-   
-// fonction pour ajouter une tache dans le DOM
-    addTodo(e) {
-        e.preventDefault();
-        if (this.state.userInput.trim() !== '') {
-            
-            const id = Math.floor(Math.random() * 24500000);
-            const nomTache = this.state.userInput;
-    
-            // Copiez le tableau existant dans le state
-            const nouveauTableau = [...this.state.tache];
-    
-            // Ajoutez le nouvel objet avec id et nomTache au tableau copié
-            nouveauTableau.push({ id: id, valeur: nomTache });
-    
-            // Mettez à jour le state avec le nouveau tableau
-            this.setState({
-                userInput: '',
-                tache: nouveauTableau
-            });
-        }
-        else{
-            alert('Remplissez la tache')
-        }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { tacheInput: "", tache: [] };
 
-    }
+    this.ajoutTache = this.ajoutTache.bind(this);
+    this.deleteTache = this.deleteTache.bind(this);
+    // this.handleChange = this.ajoutTache.bind(this);
+  }
+  ajoutTache = (e) => {
+    e.preventDefault();
+    if (this.state.tacheInput.trim() !== "") {
+      const id = Math.floor(Math.random() * 24500000);
+      const nomTache = this.state.tacheInput;
+      // Copiez le tableau existant dans le state
+      const nouveauTableau = [...this.state.tache];
 
-    //fonction qui permet de supprimer une tache ajouté
-    deleteTodo(tacheAModifierOuSupprimer) {
-        const tableau = this.state.tache;
-        const index = tableau.indexOf(tacheAModifierOuSupprimer);
-        
-        if (index !== -1) {
-            tableau.splice(index, 1);
-            this.setState({
-                tache: tableau
-            });
-        } else {
-            console.error("L'élément n'a pas été trouvé dans le tableau.");
-        }
-    }
+      // Ajoutez le nouvel objet avec id et nomTache au tableau copié
+      nouveauTableau.push({ id: id, valeur: nomTache });
 
-    //fonction qui permet d'editer une tache ajouté
-    editTodo(e, tacheAModifierOuSupprimer) {
-        
+      // Mettez à jour le state avec le nouveau tableau
+      this.setState({
+        tacheInput: "",
+        tache: nouveauTableau,
+      });
+    } else {
+      alert("Remplissez la tache");
     }
+  };
 
-    
+  deleteTache(tacheModSup) {
+    const tableau = this.state.tache;
+    const index = tableau.indexOf(tacheModSup);
 
-    //affichage au niveau du render de la liste de tableau ajouté
-    renderTodo() {
-        return this.state.tache.map((tacheAModifierOuSupprimer) => {
-            return(
-                <div 
-                key={tacheAModifierOuSupprimer.id} 
-                className="container list-group-item border-0 mx-auto w-50 ">
-                    <div className="container d-flex">
-                        <div className='col-10'>
-                        {tacheAModifierOuSupprimer.valeur}
-                        </div>
-                     <button onClick={() => this.editTodo(tacheAModifierOuSupprimer)} className='btn btn-warning mx-1'><i className="fa-regular fa-pen-to-square"></i></button> 
-                     <button onClick={() => this.deleteTodo(tacheAModifierOuSupprimer)} className='btn btn-danger mx-1'><i className="fa-solid fa-trash"></i></button>
-                    </div>
-                </div>
-            )
-        })
+    if (index !== -1) {
+      tableau.splice(index, 1);
+      this.setState({
+        tache: tableau,
+      });
+    } else {
+      console.error("L'élément n'a pas été trouvé dans le tableau.");
     }
-   
+  }
 
-    render() {
-        return <div>
-                    <h1 className="text-center my-5">Ma TodoList</h1>
-                    <form className="container w-50">
-                        <input type='text' value ={this.state.userInput} onChange={(e) => this.setState({userInput: e.target.value})} className= 'form-control' placeholder="Ajouter une tache"/>
-                        <div className= 'd-flex justify-content-center'>
-                            <button onClick ={this.addTodo} className="btn btn-primary  my-2 ">Ajouter</button>
-                        </div>
-                    </form>
-                    <div>
-                        {this.renderTodo()}
-                    </div>
-                </div> 
-     
-    }
+  handleChange = (e) => {
+    this.setState({
+      tacheInput: e.target.value,
+    });
+  };
+  render() {
+    return (
+      <div>
+        <Form
+          ajoutTache={this.ajoutTache}
+          handleChange={this.handleChange}
+          tacheInput={this.state.tacheInput}
+        />
+        <ul>
+          <Li
+            ajoutTache={this.ajoutTache}
+            handleChange={this.handleChange}
+            tacheInput={this.state.tacheInput}
+            deleteTache={this.deleteTache}
+            tache={this.state.tache}
+          />
+        </ul>
+      </div>
+    );
+  }
 }
 
+class Form extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="text-center my-5">Ma TodoList</h1>
+        <form className="container w-50" onSubmit={this.props.ajoutTache}>
+          <input
+            type="text"
+            value={this.props.tacheInput}
+            onChange={this.props.handleChange}
+            className="form-control"
+            placeholder="Ajouter une tache"
+          />
+          <div className="d-flex justify-content-center">
+            <button type="submit" className="btn btn-primary  my-2 ">
+              Ajouter
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
-    ReactDOM.render(<Todo />, document.getElementById('root'))
+class Li extends React.Component {
+  render() {
+    return this.props.tache.map((tacheModSup) => {
+      return (
+        <div
+          key={tacheModSup.id}
+          className="container list-group-item border-0 mx-auto w-50 my-3 "
+        >
+          <div className="container d-flex">
+            <div className="col-10">{tacheModSup.valeur}</div>
+            <button
+              onClick={() => this.editTodo(tacheAModifierOuSupprimer)}
+              className="btn btn-warning mx-1"
+            >
+              <i className="fa-regular fa-pen-to-square"></i>
+            </button>
+            <button
+              onClick={() => this.props.deleteTache(tacheModSup)}
+              className="btn btn-danger mx-1"
+            >
+              <i className="fa-solid fa-trash"></i>
+            </button>
+          </div>
+        </div>
+      );
+    });
+  }
+}
 
-
-
+ReactDOM.render(<App />, document.getElementById("root"));
