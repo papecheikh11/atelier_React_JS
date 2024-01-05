dd84431ed098099d8450491f321264b5b90fba68
Pape Maguette Ndiaye <papecheikh1102@gmail.com>
2024-01-05 23:54:22 +0000

    modifier

--- classe_challenge_saisie/app.js
+++ classe_challenge_saisie/app.js
@@ -6,16 +6,20 @@ class MaSaisie extends React.Component {
     };
   }
 
-handleDownload = () => {
-    const blob = new Blob([this.state.valeur], { type: "text/word" })
-    const url = URL.createObjectURL(blob)
+  handleDownload = () => {
+    const blob = new Blob([this.state.valeur], { type: "text/word" });
+    const url = URL.createObjectURL(blob);
 
     const a = document.createElement("a");
     a.href = url;
     a.download = "texte_telecharge.txt";
     a.click();
     URL.revokeObjectURL(url);
-}
+  };
+
+  handleChange = (e) => {
+    this.setState({ valeur: e.target.value });
+  };
 
   render() {
     return (
@@ -27,17 +31,42 @@ handleDownload = () => {
           </h6>
         </div>
         <div className="ma_div d-flex flex-wrap container-fluid align-content-center">
-          <div className="col-lg-8">
-            <textarea onChange={(e) => this.setState({ valeur: e.target.value })} rows='40' ></textarea>
-          </div>
-          <div className="col-lg-4 afficher">{this.state.valeur}</div>
+          <Saisie valeur={this.state.valeur} onChange={this.handleChange} />
+          <Affichage valeur={this.state.valeur} />
         </div>
         <div className="text-center my-3">
-          <button className="btn" onClick={this.handleDownload}>
-            Download text
-          </button>
+          <Button handleDownload={this.handleDownload} />
         </div>
       </div>
+    );
+  }
+}
+
+class Saisie extends React.Component {
+  render() {
+    return (
+      <div className="col-lg-8">
+        <textarea
+          onChange={this.props.onChange}
+          value={this.props.valeur}
+        ></textarea>
+      </div>
+    );
+  }
+}
+
+class Affichage extends React.Component {
+  render() {
+    return <div className="col-lg-4 afficher">{this.props.valeur}</div>;
+  }
+}
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button className="btn" onClick={this.props.handleDownload}>
+        Download text
+      </button>
     );
   }
 }
